50b140bb7cccf14216cf6e51e64ffee7827ad1ec
Sawadogo <cheick.linux@gmail.com>
2022-12-23 15:24:19 +0000

    Defis Week4 day1

--- Week-4/Day-1/Defis/Defis-day1.py
+++ Week-4/Day-1/Defis/Defis-day1.py
@@ -1,7 +1,19 @@
+'''	#1-
 chaine=input("Entrer une chaine de caractere contenant 10 caractere: ")
-if len(chaine)>10:
+taille=len(chaine)
+if taille>10:
 	print('chaine trop longue')
-elif len(chaine)<10:
+elif taille<10:
 	print("chaîne pas assez longue")
 else:
 	print("votre chaine contient exactement 10 caractere")
+
+	#2-
+print(f"le premier caractere de votre chaine est {chaine[0]}")
+print(f"le dernier caractere de votre chaine est {chaine[taille-1]}")
+
+	#3-
+for lettre in range(chaine):
+	print(chaine[0:lettre])
+	
+'''
