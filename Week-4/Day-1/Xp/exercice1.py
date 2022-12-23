9aadfe7d7cd1ee1fa73e01e6804a1a894460b5b8
Sawadogo <cheick.linux@gmail.com>
2022-12-23 13:46:54 +0000

    Exercice XP Week4 day1

--- Week-4/Day-1/Xp/exercice1.py
+++ Week-4/Day-1/Xp/exercice1.py
@@ -1,4 +1,97 @@
+"""
+ #Exercice 1 : Bonjour Le Monde
 print('Hello Word')
 print('Hello Word')
 print('Hello Word')
 print('Hello Word')
+"""
+
+"""
+	#Exercice 2 : Quelques Maths
+print((99^3) * 8)
+
+"""
+
+
+
+"""
+	#Exercice 3 : Quelle Est La Sortie ?
+False
+True
+False
+TypeError: '>' not supported between instances of 'int' and 'str'# impossible de comparer un entier avec une chaine de caractere
+False
+
+
+"""
+
+"""
+	#🌟 Exercice 4 : Votre Marque D'ordinateur
+
+computer_brand="HP Pavillon Laptop 15-cc6xx"
+print(f"I have a {computer_brand} computer.")
+
+
+"""
+
+"""
+	#🌟 Exercice 5 : Vos Informations
+
+name="Sawadogo Mohamed"
+age=22
+shoe_size=42
+info=f"Je me nomme {name} j'ai {age}ans et j'ai une collection de Air-max numero {shoe_size}."
+print(info)
+
+
+"""
+
+
+"""
+	#🌟 Exercice 6 : A & B
+a=15
+b=10
+if a>b:
+	print('Hello Word')
+
+"""
+
+
+
+"""
+	#Exercice 7 : Pair Ou Impair
+
+nombre=int(input("Entrer un nombre"))
+if nombre%2==0 :
+	print(f"{nombre} est un nombre paire")
+else:
+	print(f"{nombre} est un nombre impaire")
+
+
+"""
+
+
+"""
+	#🌟 Exercice 8 : Comment T'appelles-Tu ?
+
+mon_nom="MOHAMED"
+nom_utilisateur=input("Entrer votre nom: ")
+nom_utilisateur=nom_utilisateur.upper()
+if mon_nom == nom_utilisateur:
+	print("comment tu vas mon homo")
+else:
+	print("mon nom est plus jolie que le tien")
+"""
+
+
+"""
+	#🌟 Exercice 9 : Assez Grand Pour Faire Des montagnes Russes
+
+taille=int(input("Entrer votre taille en pouce 'P': "))
+if taille*2.54 > 145:
+	print("Vous etes assez grand pour faire la montagne Russes")
+else:
+	print("Desole vous etes trop petit pour les montagnes Russe")		
+
+
+"""
