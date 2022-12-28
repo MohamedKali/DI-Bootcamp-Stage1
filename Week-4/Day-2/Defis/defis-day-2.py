ec2b530afd8b13be50da9995c183e41bd531490d
Sawadogo <cheick.linux@gmail.com>
2022-12-28 22:09:48 +0000

    exercice jour 2

---
+++ Week-4/Day-2/Defis/defis-day-2.py
@@ -0,0 +1,16 @@
+#Défi 1
+'''nombre=int(input("Entrer un nombre dont vous voulez les mutiples: "))
+lenght=int(input("Entrer le nombre de multiple a obtenir:\n"))
+multiple=[]
+for i in range(1,lenght+1):
+	multiple.append(nombre*i)
+
+print(f"nombre: {nombre} - taille: {lenght} ➞ {multiple}\n")'''
+
+#Défi 2
+'''
+chaine=input("Entrer un mot: ")
+for i in chaine:
+	chaine=chaine.replace(i+i,i)
+print(f"nouvelle chaine: {chaine}")
+'''
--- Week-4/Day-2/Xp/Xp-day-2.py
+++ Week-4/Day-2/Xp/Xp-day-2.py
@@ -1,4 +1,28 @@
 '''
+🌟 Exercice 1 : Set
+
+my_fav_num=set({'66-00-18-05','66-18-19-20','72-56-20-24'})
+
+my_fav_num.add('66-18-87-20')
+my_fav_num.add('72-56-56-24')
+print(my_fav_num)
+my_fav_num.remove('66-00-18-05')
+print(f"\nmy_fav_num avec le dernier numero supprimer {my_fav_num}\n")
+my_friend_fav=set({'64-87-18-05','73-53-03-20','68-32-03-20','73-24-99-20'})
+print(f"my_friend_fav: {my_friend_fav}\n")
+our_fav_numbers=set(list(my_fav_num)+list(my_friend_fav))
+print(f"mon nouveau ensemble (mes contact favoris + de mon ami): {our_fav_numbers}\n")
+
+'''
+
+#🌟 Exercice 2 : Tuple
+
+	#Il n'est pas possible d'ajouter un element car les tuples sont imuables.
+
+
+
+
+'''
 			#Exercice 3: List
 
 basket=['Banana','Apples','Orange','BlueBerries']
@@ -47,13 +71,95 @@ for i in range(1,20):
 '''
 
 
-#'''
+'''
 	#Exercice6: En boucle
 mon_nom='Mohamed'
-nom=input("Entrer votre nom").capitalize()
-while nom is not mon_nom:
-	nom=input("Entrer votre nom")
-print(f"nous avons le meme nom Mr{nom}")
+nom=input("Entrer votre nom: ").capitalize()
+while nom != mon_nom:
+	nom=input("Entrer votre nom: ")
+print(f"nous avons le meme nom Mr {nom}") 
+
+'''
+
+
+"""
+#Exercice 7: fruit prefere
+
+fruit_prefere=input("Entrer vos fruits prefere sur une ligne ex:( mange goyave banane): ").capitalize()
+liste_fruit_prefere=fruit_prefere.split()
+fruit=input("Entrer le nom d'un fruit: ").capitalize()
+if fruit in liste_fruit_prefere:
+	print("Vous avez choisir un de vos fruits preferes! profitez-en!")
+else:
+	print("Vous avez choisi un nouveau fruit. J'espere que vous apprecierez")
+print(liste_fruit_prefere)
+
+"""
+
+
+"""
+	#Exercice 8: Qui a commander une pizza!
+
+graniture=[]
+grani=input("Entrer les graintures de votre pizza (une fois terminer taper \"Quit\" pour enregistrer): ").capitalize()
+while grani != 'Quit' :
+	if grani in graniture:
+		print(f"{grani} deja presente dans votre pizza")
+	else:
+		graniture.append(grani)
+		print(f"vous venez d'ajouter {grani} a votre pizza")
+	grani=input("graintures suivante: (une fois terminer taper \"Quit\" pour enregistrer): ").capitalize()
+	#if grani =="Quit"
+
+print('Liste des graintures presente dans votre pizza :')
+print('----------------------------------------------')
+for x in graniture:
+	print(f" {x}")
+print(f"Le prix total de votre pizza est de: {10 + 2.5*len(graniture)}$")
+"""
+
+'''
+		#Exercice 9 : Cinémax
+from os import system
+import time
+liste_age=[]
+prix_total=0
+age=int(input("Enrer l'age de tous les membre de la famille (taper '1000' pour enregistrer une fois tous les ages saisir): "))
+while age != 1000:
+	liste_age.append(age)
+	if age <3:
+		pass
+	elif age >=3 and age<12:
+		prix_total+=10
+	else:
+		prix_total+=15
+	age=int(input("Enrer l'age du membre suivant (taper '1000' pour enregistrer une fois tous les ages saisir): "))
+
+
+print(f"le cout total des tickets pour votre famille est de: {prix_total}$")
+system('pause')
+system('cls')
 
 
 
+# 4-
+print ("Question 4: Exercice 9\n\n")
+liste_nom=[]
+nom=input("Entrer vos noms ('save' pour enregistrer la liste): ").capitalize()
+while nom != 'Save':
+	liste_nom.append(nom)
+	nom=input("personne suivante ('save' pour enregistrer la liste): ").capitalize()
+
+for x in liste_nom:
+	age=int(input(f"Mr {x} quel est votre age: "))
+	if age <16:
+		print(f"Mr {x} vous n'etes pas autorisez a reagarder ce film veillez sortir svp !!!")
+		liste_nom.remove(x)
+	else:
+		print(f"desole du derangement Mr {x} ")
+
+print("liste des personnes autorisez a suivre cet film")
+print("-----------------------------------------------")
+for x in liste_nom:
+	print( x)
+'''
